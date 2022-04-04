d1641818a33e83ebc8ce4a70c221ade00a7fa455
Пашин Дмитрий <ykrop1314@gmail.com>
2022-04-05 02:23:43 +0500

    2

--- todo/urls.py
+++ todo/urls.py
@@ -4,8 +4,12 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    # роут для отображения страницы - Регистрация
+    # роут для отображения страницы - Регистрация нового пациента
     path('reg', views.reg, name='reg'),
-    # роут регистрации пользователя
+    # роут Домашней страницы
     path('', views.index, name='index'),
+    # роут для отображения страницы - Дата обращения
+    path('dateof', views.dateof, name='dateof'),
+    # роут для отображения страницы - Сотрудники
+    path('employes', views.employes, name='employes'),
 ]
